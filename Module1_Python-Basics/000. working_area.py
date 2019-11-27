ac898219f8352cd87d07ef7969c91ff29d7ff85d
Mr Louis F <fru.louis@gmail.com>
2019-11-27 09:28:20 -0600

    This is commit from youtube tutorial. 100. introduction to python. DONE.

---
+++ Module1_Python-Basics/000. working_area.py
@@ -0,0 +1,89 @@
+#ENGLISH: Write a program that prints the word "Hello, World!" on the screen.
+#Syntax, Grammar, Vocabulary. #Formal Languages  #Python intepreter
+# - python,java, sql, ruby, php
+
+#0.0.1: Hello world and comments.
+#This line below, prints hello world on the screen.
+#print("Hello, World!") #This is a comment.
+# This is a comment 1.
+# This is a comment 2.
+# This is a comment 3.
+
+"""
+This is a comment 1.
+This is a comment 2.
+This is a comment 3.
+"""
+
+#0.0.2 Print numbers,strings and variables.
+#Data types: String, Number, Boolean
+#Print the number 5 on the screen.
+#print("Hello, World!")
+#print("five 1")
+#print('five 2')
+#print(5)
+##Print my age of 25 on the screen.
+#age = 30  #Variable #Setting Variables #Operators.
+#print(age+1)
+
+#0.0.3 Printing Strings and Setting Variables.
+#Write a program that takes my name, and greets me by saying hello.
+
+#my_name = 'Fru'
+#print("Hello there, " + my_name + "!")  #Concatenation. #concat
+
+#first_name = "Fru"
+#middle_name = "Louis"
+#last_name = "Nde"
+#print(first_name+" "+middle_name+" "+last_name)
+
+#first_name, middle_name, last_name = "Fru", "Louis", "Nde"
+#print(first_name+" "+middle_name+" "+last_name)
+
+#first_name = middle_name = last_name = "Fru"
+#print(first_name+" "+middle_name+" "+last_name)
+
+#0.0.4: More variables, Strings, concats.
+#my_name = "Fru" #String
+#my_age = 30 #number
+#print("Hello there, my name is " + my_name + " and my age is " + my_age)
+
+#0.0.5: Booleans and Conditionals. bool
+#True or False.
+#5 and 8: Is 5 > 8 ?
+#print(5 > 8) #FALSE
+#print(5 > 3) #TRUE
+#print(5 == 5) #FALSE
+#print(bool("Fru")) #TRUE
+#print(bool(3)) #TRUE
+
+#0.0.6: Conditionals and Boolean logic.
+#my_age = 30
+#your_age = 57
+
+#if my_age > your_age:
+#    print("I am older than you!")
+#else:
+#    print("You are older than me!")
+
+#0.0.7: Python Indentation.
+my_age = 30
+your_age = 57
+if my_age > your_age: #block 1
+    print("I am older than you!")
+else: #block 2
+    print("You are older than me!")
+
+#0.0.8: Executing python scripts in files.
+
+
+
+
+
+
+
+
+
+
+
+
